5aa9796acbab56416c02b9df589a0b517012f268
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-07-05 22:13:08 +0200

    update

--- RNGame/components/PrimaryButton.js
+++ RNGame/components/PrimaryButton.js
@@ -1,12 +1,43 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 const PrimaryButton = ({ children }) => {
+  function pressHanlder() {
+    console.log("Button pressed");
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={pressHanlder}
+        android_ripple={{ color: "#a00854" }}
+        style={({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  }
+});
--- RNGame/screens/StartGameScreen.js
+++ RNGame/screens/StartGameScreen.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -13,3 +19,29 @@ const StartGameScreen = () => {
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#440424",
+    borderRadius: 8,
+    elevation: 4, // Android only shadow
+    shadowColor: "black", // iOS only shadow
+    shadowOffset: { width: 0, height: 2 }, // iOS only shadow
+    shadowRadius: 6, // iOS only shadow
+    shadowOpacity: 0.25, // iOS only shadow
+  },
+  numberInput: {
+    width: 50,
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
