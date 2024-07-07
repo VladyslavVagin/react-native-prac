69e6567f0c9e3aa6e96fd467d506c53f794287dd
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-07-07 22:05:26 +0200

    update Overview Meal

--- RNMealApp/screens/CategoriesScreen.js
+++ RNMealApp/screens/CategoriesScreen.js
@@ -1,15 +1,26 @@
+// @ts-nocheck
 import React from "react";
 import { FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
-function renderCategoryItem(itemData) {
-  return (
-    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
-  );
-}
+const CategoriesScreen = ({ navigation }) => {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      });
+    }
+  
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
 
-const CategoriesScreen = () => {
   return (
     <FlatList
       data={CATEGORIES}
