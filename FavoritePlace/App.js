0275b13a401a6d29646f2aadd49577f06810a77e
VladyslavVagin <vladtwince@gmail.com>
2024-07-14 15:23:02 +0200

    update

--- FavoritePlace/App.js
+++ FavoritePlace/App.js
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import IconButton from "./components/UI/IconButton";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
+import { Colors } from "./constants/colors";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,11 +15,20 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: Colors.primary500,
+            },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
           <Stack.Screen
             name="AllPlaces"
             component={AllPlaces}
             options={({ navigation }) => ({
+              title: "Favorite Places",
               headerRight: ({ tintColor }) => (
                 <IconButton
                   icon="add"
@@ -29,7 +39,13 @@ export default function App() {
               ),
             })}
           />
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{
+              title: "Add a New Place",
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ FavoritePlace/components/Places/ImagePicker.js
@@ -0,0 +1,87 @@
+// @ts-nocheck
+import React, { useState } from "react";
+import { View, Alert, Image, Text, StyleSheet } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
+import OutlinedButton from "../UI/OutlinedButton";
+import { Colors } from "../../constants/colors";
+
+const ImagePicker = () => {
+  const [pickedImage, setPickedImage] = useState(null);
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+    console.log(pickedImage)
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient permissions!",
+        "You need to grant camera permissions to use this app."
+      );
+      return false;
+    }
+    return true;
+  }
+
+  async function takeImageHandler() {
+    try {
+        const hasPermission = await verifyPermissions();
+        if (!hasPermission) {
+          return;
+        }
+        const image = await launchCameraAsync({
+          allowsEditing: true,
+          aspect: [16, 9],
+          quality: 0.5,
+        });
+
+        console.log(image);
+        setPickedImage(image.assets[0].uri);
+    } catch (error) {
+        console.log(error);
+    }
+  }
+
+  let imagePreview = <Text>No image picked yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image source={{ uri: pickedImage }} style={styles.image} />;
+  }
+
+  return (
+    <View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take Image
+      </OutlinedButton>
+    </View>
+  );
+};
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
